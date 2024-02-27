12e5cb5a54c7e3c1f9763d8b2f51aa4a1d2350f5
uyrabi <uny050102@gmail.com>
2024-02-27 20:57:52 +0900

    key問題修正

--- client/routes/posts/postList.tsx
+++ client/routes/posts/postList.tsx
@@ -1,5 +1,6 @@
 import { ScrollArea } from "@shadcn/scroll-area";
 import { Separator } from "@shadcn/separator";
+import React from "react";
 
 export function PostList(data) {
 	return (
@@ -7,12 +8,12 @@ export function PostList(data) {
 			<div className="p-4">
 				<h4 className="mb-4 text-sm font-medium leading-none">Posts</h4>
 				{data.posts.map((post) => (
-					<>
-						<div key={post.id} className="text-sm">
+					<React.Fragment key={post.id}>
+						<div className="text-sm">
 							{post.title}:{post.content}
 						</div>
 						<Separator className="my-2" />
-					</>
+					</React.Fragment>
 				))}
 			</div>
 		</ScrollArea>
