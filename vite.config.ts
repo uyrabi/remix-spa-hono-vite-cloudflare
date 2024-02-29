7b409eb43e09f8571dea5feb29fa3c203f8fca26
uyrabi <uny050102@gmail.com>
2024-02-29 10:00:42 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+console.log("=== vite config ===");
+
+console.log(`PORT: ${process.env.SERVER_PORT}`);
+console.log(`API: http://localhost:${process.env.SERVER_PORT}`);
+
 export default defineConfig({
 	plugins: [remix({ appDirectory: "client", ssr: false }), tsconfigPaths()],
 	server:
@@ -10,10 +15,11 @@ export default defineConfig({
 			? {
 					proxy: {
 						// `/api` へのリクエストを `localhost:8788` に転送
-						"/api": "http://localhost:8788",
-						// "/api": `http://localhost:${process.env.SERVER_PORT}`,
+						// "/api": "http://localhost:8788",
+						"/api": `http://localhost:${process.env.SERVER_PORT}`,
 					},
 					host: true,
+					port: process.env.CLIENT_PORT,
 					strictPort: true,
 			  }
 			: {},
